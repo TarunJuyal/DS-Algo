bf8880dcd73079da6dd8af5bb8cf4b5e3d62be13
Tarun Juyal <46627661+TarunJuyal@users.noreply.github.com>
2020-02-02 09:34:55 +0530

    Add files via upload

---
+++ missingNumber.java
@@ -0,0 +1,24 @@
+
+public class missingNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[]= {0,1,2,3,5};
+        int max=arr[0];
+     for(int i =0;i<=arr.length-1;i++) {
+     if(arr[i]>max)
+      max=arr[i];
+     }
+    //System.out.println(max);
+     int[] a=new int[max+1];
+   for(int i =0;i<=arr.length-1;i++) {
+  a[arr[i]]++;
+   }
+  for(int i =0;i<=a.length-1;i++) {
+  if(a[i]==0) {
+  System.out.println(i +"is missing in array");
+ }
+ }
+	}
+
+}
