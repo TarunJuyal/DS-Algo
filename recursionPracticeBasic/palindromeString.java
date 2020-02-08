196d086740c51edb13b61e500d4b3f597a48df7c
Tarun Juyal <46627661+TarunJuyal@users.noreply.github.com>
2020-02-08 10:51:00 +0530

    Add files via upload

---
+++ recursionPracticeBasic/palindromeString.java
@@ -0,0 +1,23 @@
+package recursionPracticeBasic;
+
+public class palindromeString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s="abcdcba";
+       //String str=String.copyValueOf(findPalindrome(s,0,s.length()));
+		if(findPalindrome(s, 0, s.length()-1))
+			System.out.println("Palindrome String");
+		else
+			System.out.println("Not Palindrome");
+	}
+	public static boolean findPalindrome(String s, int first,int last) {
+		if(first==last)
+			return true;
+		char a[]=s.toCharArray();
+		if(a[first]!=a[last])
+			return false;
+		return findPalindrome(s,first+1, last-1);
+		
+	}
+}
