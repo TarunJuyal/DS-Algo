2c836b7889a35b31773b189b2617c5222f03eb1d
Tarun Juyal <tarunJuyalHere@gmail.com>
2020-04-18 09:39:22 +0530

    Add files via upload

---
+++ tree/BinaryTree01.java
@@ -0,0 +1,90 @@
+package tree;
+
+// Includes Height of tree , Size of tree(Number of nodes in a tree) and search an element in tree.
+
+import java.util.Scanner;
+
+class BTNode{
+	int data;
+	BTNode left;
+	BTNode right;
+	BTNode(int data){
+		this.data=data;
+	}
+}
+class BTOperations{
+	Scanner s=new Scanner(System.in);
+	String node="root";
+	BTNode insert() {
+		System.out.println("Enter data of "+node);
+		int data=s.nextInt();
+		if(data==-1)
+			return null;
+		BTNode treeNode=new BTNode(data);
+		node="left";
+		treeNode.left=insert();
+		node="right";
+		treeNode.right=insert();
+		return treeNode;
+	}
+	void print(BTNode root) {
+		if(root==null)
+			return;
+		String output="";
+		output+=root.data+" => ";
+		if(root.left!=null)
+			output+="Left "+root.left.data;
+		if(root.right!=null)
+			output+="Right "+root.right.data;
+		System.out.println(output);
+		print(root.left);
+		print(root.right);
+	}
+	int getHeight(BTNode root) {
+		if(root==null)
+			return 0;
+		int currentHeightLeft=getHeight(root.left);
+		int currentHeightRight=getHeight(root.right);
+		if(currentHeightLeft>currentHeightRight)
+			return currentHeightLeft+1;
+		else
+			return currentHeightRight+1;
+		
+	}
+	int getSize(BTNode root) {
+		if(root==null)
+			return 0;
+		int currentSize=0;
+		currentSize+=getSize(root.left);
+		currentSize+=getSize(root.right);
+		return currentSize+1;
+	}
+	boolean searchElement(BTNode root,int value) {
+		if(root==null)
+			return false;
+		if(root.data==value)
+			return true;
+		boolean temp=searchElement(root.left, value);
+		if(temp)
+			return true;
+		return searchElement(root.right, value);
+		
+	}
+}
+public class BinaryTree01 {
+	static Scanner scanner=new Scanner(System.in);
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BTOperations b=new BTOperations();
+		BTNode root=b.insert();
+		b.print(root);
+		System.out.println("Height is: "+b.getHeight(root));
+		System.out.println("Number of node or size is: "+b.getSize(root));
+		System.out.println("Enter value to be searched: ");
+	    int value=scanner.nextInt();
+	    if(b.searchElement(root,value))
+	    	System.out.println(value+" present in the tree.");
+	    else
+	    	System.out.println(value+" not present in tree");
+	}
+}
