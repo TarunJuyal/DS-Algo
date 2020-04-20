3e67efdf9e836b66ebabb2c5d815da5dec71c103
Tarun Juyal <tarunJuyalHere@gmail.com>
2020-04-20 08:13:17 +0530

    Create DetectLoopAndRemoveInLinkedList.java

---
+++ linkedList/DetectLoopAndRemoveInLinkedList.java
@@ -0,0 +1,139 @@
+package linkedList;
+
+class Node<T>{
+	T data;
+	Node<T> next;
+	Node(T data){
+		this.data=data;
+	}
+}
+class LinkedListOperations<T>{
+	Node<T> start;
+	void insertAtBeginning(Node<T> node) {
+		if(start==null) {
+			start=node;
+			return;
+		}
+		node.next=start;
+		start=node;
+	}
+	void insertAtEnd(Node<T> node) {
+		if(start==null) {
+			start=node;
+			return;
+		}
+		Node<T> temp=start;
+		while(temp.next!=null) {
+			temp=temp.next;
+		}
+		temp.next=node;
+	}
+	void insertAtPosition(Node<T> node,int position) {
+		if(start==null) {
+			start=node;
+			return;
+		}
+		if(position==0) {
+			node.next = start;
+			start = node;
+			return ;
+		}
+		if(position>0) {
+			Node<T> temp = start;
+			int i = 1;
+			while(i<position) {
+				temp = temp.next;
+				i++;
+			}
+			node.next = temp.next;
+			temp.next  = node;
+		}
+	}
+	void print() {
+		Node<T> temp  = start;
+		while(temp!=null) {
+			System.out.println(temp.data);
+			temp = temp.next;
+		}
+	}
+	void delete(T data) {
+		Node<T> temp=start;
+		if(start==null) {
+			System.out.println("Linked List is Empty!!");
+			return;
+		}
+		if(start.data==data) {
+			start=start.next;
+			return;
+		}
+		Node<T> previous=start;
+		while(temp.next!=null) {
+			if(temp.data==data) {
+				previous.next = temp.next;
+			}
+			previous=temp;
+			temp=temp.next;
+		}
+	}
+	//Floyd Cycle Detection Algo or Hare and tortoise
+	void detectLoop() {
+		Node<T> slow;
+		Node<T> fast;
+		slow=fast=start;
+		boolean flag=false;
+		while(fast!=null && fast.next!=null) {
+			slow=slow.next;
+			fast=fast.next.next;
+			if(slow==fast) {
+				System.out.println("Loop in LinkedList detected");
+				flag=true;
+				break;
+			}
+		}
+		if(!flag)
+			System.out.println("No loop in LinkedList");
+	}
+	 void detectAndRemoveLoopInLL(){
+		 Node<T> slow = start;
+		 Node<T> fast = start;
+		 while(fast!=null && fast.next!=null) {
+			 slow = slow.next;
+			 fast = fast.next.next;
+			 if(slow==fast) {
+				 break;
+			 }
+		 }
+		 if(slow!=fast) {
+			 System.out.println("No Loop Found....");
+			 return ;
+		 }
+		 Node<T> loopPoint=slow;
+		 while(slow.next!=loopPoint) {
+			 slow=slow.next;
+		 }
+		 slow.next=null;
+			
+	 }
+public class GenericLinkedList {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LinkedListOperations<String> ll=new LinkedListOperations<>();
+		Node<String> node = new Node<String>("jack");
+		ll.insertAtEnd(node);
+		ll.insertAtEnd(new Node<String>("john"));
+		ll.insertAtEnd(new Node<String>("paul"));
+		ll.insertAtEnd(new Node<String>("kim"));
+		ll.insertAtBeginning(new Node<String>("timothy"));
+		int pos = 2;
+		ll.insertAtPosition(new Node<String>("howard"), pos -1);
+		ll.print();
+		System.out.println("********************************");
+		ll.start.next.next.next.next.next.next=ll.start;
+		ll.detectLoop();
+		ll.detectAndRemoveLoopInLL();
+		ll.print();
+		
+	}
+	
+
+}
