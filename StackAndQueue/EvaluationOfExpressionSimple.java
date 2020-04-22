5513b01f79b6d94781a1c8cae5e8218564cda6ec
Tarun Juyal <tarunJuyalHere@gmail.com>
2020-04-22 18:07:35 +0530

    Add files via upload

---
+++ StackAndQueue/EvaluationOfExpressionSimple.java
@@ -0,0 +1,43 @@
+package StackAndQueue;
+
+import java.util.Stack;
+
+public class EvaluationOfExpressionSimple {
+	static Stack<Integer> operandStack=new Stack<>();
+	static int calculate(int firstOp,char Op,int secondOp) {
+		int result=0;
+		//System.out.println("operator"+Op);
+		if(Op=='+')
+			result=firstOp+secondOp;
+		if(Op=='-')
+			result=firstOp-secondOp;
+		if(Op=='*')
+			result=firstOp*secondOp;
+		if(Op=='/')
+			result=firstOp/secondOp;
+		//System.out.println("currentResult"+result);
+		return result;
+	}
+	static int evaluate(char s[]) {
+		operandStack.push(Character.getNumericValue(s[0])); //first element has to be a number
+		int i=1;
+		while(i<s.length) {
+			if(s[i]=='+'||s[i]=='-'||s[i]=='*'||s[i]=='/') {
+					int operand=Character.getNumericValue(s[i+1]);
+					//System.out.println("current"+s[i]);
+					operandStack.push(calculate((int)operandStack.pop(),s[i],operand));
+					i+=2;
+				}
+			else 
+				i++;
+		}
+		return operandStack.pop();
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s="2*4-6+5/7+1*2+4";
+		System.out.println("Given Expression: "+s);
+		System.out.println("Result: "+evaluate(s.toCharArray()));
+	}
+
+}
