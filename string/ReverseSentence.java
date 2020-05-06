9bb60f3b8e81eac21a638b081d6055cf1bfc6d11
Tarun Juyal <tarunJuyalHere@gmail.com>
2020-05-06 11:36:30 +0530

    Update ReverseSentence.java

--- string/ReverseSentence.java
+++ string/ReverseSentence.java
@@ -1,19 +1,6 @@
 package string;
 
 public class ReverseSentence {
-//	static String reverseWords(String s) {
-//		int i=0; 
-//		System.out.println(s);
-//		if(s.length()==0)
-//			return "";
-//		while(s.charAt(i)!=' '||i==s.length()) {
-//			i++;
-//			if(i==s.length()-1)
-//				break;
-//		}
-//		System.out.println("i is: "+i+"word is: "+s.substring(0, i));
-//		return  reverseWords(s.substring(i+1))+" "+ s.substring(0, i);
-//	}
 	static String reverseWords(String s) {
 		String result="";
 		String str[]=s.split(" ");
