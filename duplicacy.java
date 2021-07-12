06aea64cb42d1a78379442a253e40a309069d946
Tarun Juyal <tarunJuyalHere@gmail.com>
2021-07-12 11:44:57 +0530

    Update duplicacy.java

--- duplicacy.java
+++ duplicacy.java
@@ -1,3 +1,5 @@
+// brute force n^2 nested loop
+
 //Using Hashing
 
 public class duplicacy {
@@ -23,4 +25,48 @@ public class duplicacy {
 
 	}
 
+}
+
+// If given input in natural sequence and only one duplicate element then can use , Sum of given elements - Sum of natural sequence
+
+/* If given input in natural sequence then each value will have its own corresponding index. 
+Iterate over array and mark the value at index which is current value of current index as negative of itself.
+If already negative value encountered that means that element is duplicate. */
+
+public int repeatedNumber(List<Integer> list) {
+  if (list.size() <= 1) {
+    return -1;
+  }
+
+  for (int i = 0; i < list.size(); i++) {
+    if (list.get(Math.abs(list.get(i))) > 0) {
+      list.set(Math.abs(list.get(i)), -1 * list.get(Math.abs(list.get(i))));
+    } else {
+      return Math.abs(list.get(i));
+    }
+  }
+
+  return 0;
+}
+
+// an approach like Floyd Cycle Detection Algo. if given in natural sequence
+
+public int repeatedNumber(List<Integer> list) {
+  if (list.size() <= 1)
+    return -1;
+
+  int slow = list.get(0);
+  int fast = list.get(list.get(0));
+
+  while (fast != slow) {
+    slow = list.get(slow);
+    fast = list.get(list.get(fast));
+  }
+
+  slow = 0;
+  while (fast != slow) {
+    slow = list.get(slow);
+    fast = list.get(fast);
+  }
+  return slow;
 }
