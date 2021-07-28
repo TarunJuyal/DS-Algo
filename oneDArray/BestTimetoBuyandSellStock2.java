9b2779b731fc1718eee0fb152b72b7d2f2272d6e
Tarun Juyal <tarunJuyalHere@gmail.com>
2021-07-28 16:28:58 +0530

    Create BestTimetoBuyandSellStock2.java

---
+++ oneDArray/BestTimetoBuyandSellStock2.java
@@ -0,0 +1,52 @@
+/*
+Can buy and sell multiple times.
+*/
+
+// 1. Peak Valley approach O(n)
+class Solution {
+    public int maxProfit(int[] prices) {
+        int i = 0;
+        int valley = prices[0];
+        int peak = prices[0];
+        int maxprofit = 0;
+        while (i < prices.length - 1) {
+            while (i < prices.length - 1 && prices[i] >= prices[i + 1])
+                i++;
+            valley = prices[i];
+            while (i < prices.length - 1 && prices[i] <= prices[i + 1])
+                i++;
+            peak = prices[i];
+            maxprofit += peak - valley;
+        }
+        return maxprofit;
+    }
+}
+
+// 2. Single pass
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxprofit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1])
+                maxprofit += prices[i] - prices[i - 1];
+        }
+        return maxprofit;
+    }
+}
+
+                //OR
+class Solution {
+    public int maxProfit(int[] prices) {
+        int total=0;
+        int minprice = Integer.MAX_VALUE;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minprice)
+                minprice = prices[i];
+            else if (prices[i] - minprice > 0){
+                total+=(prices[i] - minprice);
+                minprice=prices[i];
+            }
+        }
+        return total;
+    }
+}
