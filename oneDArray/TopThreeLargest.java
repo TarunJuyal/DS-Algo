6431775b434d1811570dac615bafe62acb98c558
Tarun Juyal <tarunJuyalHere@gmail.com>
2021-07-29 17:08:18 +0530

    Update TopThreeLargest.java

--- oneDArray/TopThreeLargest.java
+++ oneDArray/TopThreeLargest.java
@@ -9,18 +9,18 @@ public class TopThreeLargest {
 		int a[]= {1,2,3,4,7,17,6,9,8};
 		int first=Integer.MIN_VALUE,second=Integer.MIN_VALUE,third=Integer.MIN_VALUE;
 		for(int i=1;i<a.length;i++) {
-			if(a[i]>first) {
-				third=second;
-				second=first;
-				first=a[i];
-				}
+			if(a[i]>third) {
+				third=a[i];
+			}
 			else if(a[i]>second) {
 				third=second;
 				second=a[i];
-			}  
-			else if(a[i]>third) {
-				third=a[i];
 			}
+			else if(a[i]>first) {
+				third=second;
+				second=first;
+				first=a[i];
+			}  
 		}
 		System.out.println("first largest is: "+first);
 		System.out.println("second largest is: "+second);
