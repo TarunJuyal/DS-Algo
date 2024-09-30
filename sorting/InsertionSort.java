71b7bfcbd76aad0db5560863e2aa1aee6540cf46
Tarun Juyal <tarunJuyalHere@gmail.com>
2024-09-30 15:17:09 +0530

    Update InsertionSort.java

--- sorting/InsertionSort.java
+++ sorting/InsertionSort.java
@@ -1,24 +1,34 @@
 package sorting;
 
-//pick a element and put it in its correct position in the already traversed part of array by shifting.
-
 public class InsertionSort {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int a[]= {2,4,3,7,6,9,5,8};
-		for(int i=1;i<a.length;i++) {
-			int temp=a[i];
-			int j=i-1;
-			while(j>=0 && a[j]>temp) {
-				a[j+1]=a[j];
-				j--;
-			}
-			a[j+1]=temp;;
-		}
-		System.out.println("Sorted array: ");
-		for(int i:a)
-			System.out.print(i+" ");
-	}
+    public static void insertionSort(int[] a){
+        for (int i = 1; i < a.length; i++) {
+            int currentItem=a[i];
+            int j=i-1;
+            while(j>=0 && a[j] > currentItem) {
+                a[j+1]=a[j];
+                j--;
+            }
+            a[j+1]=currentItem;
+        }
+    }
 
+    private static int[] insertionSortRecursive(int[] a, int i, int j){
+        //  base condition
+        if(i==a.length) return a;
+
+        int currentItem=a[i];
+        while(j>=0 && a[j]>currentItem) {
+            a[j+1]=a[j];
+            j--;
+        }
+        a[j+1]=currentItem;
+
+        return insertionSortRecursive(a,i+1,i);
+    }
+
+    public static int[] insertionSortRecursive(int[] a){
+        return insertionSortRecursive(a,1,0);
+    }
 }
