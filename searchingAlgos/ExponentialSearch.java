dd4193456ea4c41a8006a573b8967884531a3c14
Tarun Juyal <tarunJuyalHere@gmail.com>
2024-09-30 17:02:31 +0530

    Update ExponentialSearch.java

--- searchingAlgos/ExponentialSearch.java
+++ searchingAlgos/ExponentialSearch.java
@@ -22,11 +22,4 @@ public class ExponentialSearch {
 		else
 			return BinarySearch(a, target, low, mid-1);
 	}
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int[] a= {10,20,40,50,70,80};             //only works on sorted array.
-		System.out.println(search(a, 10));
-	}
-
 }
