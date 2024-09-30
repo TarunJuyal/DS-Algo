9f4e6ff5abe77a94a3daa49aa331782daea57c74
Tarun Juyal <tarunJuyalHere@gmail.com>
2024-09-30 16:58:59 +0530

    Update JumpSearch.java

--- searchingAlgos/JumpSearch.java
+++ searchingAlgos/JumpSearch.java
@@ -1,11 +1,9 @@
 package searchingAlgos;
 
-//time complexity : root of n
-
 public class JumpSearch {
 
 	public static int search(int[] a, int target) {
-		int blocksize=(int)Math.sqrt(target);
+		int blocksize=(int)Math.sqrt(a.length);
 		int start=0,next=blocksize;
 		while(start<a.length && a[next-1]<target) {
 			start=next;
@@ -16,17 +14,10 @@ public class JumpSearch {
 		return  LinearSearch(a, target, start,next);
 	}
 	
-	public static int LinearSearch( int[] a, int target,int start,int end) {
+	private static int LinearSearch( int[] a, int target,int start,int end) {
 		for(int i=start;i<end;i++)
 			if(a[i]==target)
 				return i;
 		return -1;
 	}
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int[] a= {10,20,40,50,70,80};             //only works on sorted array.
-		System.out.println(search(a, 80));
-	}
-
 }
