6b1b6ac6e73c41ef61eb89e5d780de769bf78569
Tarun Juyal <tarunJuyalHere@gmail.com>
2024-09-30 16:36:23 +0530

    Update BucketSort.java

--- sorting/BucketSort.java
+++ sorting/BucketSort.java
@@ -5,31 +5,28 @@ import java.util.Collections;
 import java.util.List;
 
 public class BucketSort {
+    private static int maxValue(int[] a){
+        int max=Integer.MIN_VALUE;
+        for(int i: a){
+            if(i>max) max=i;
+        }
+        return max;
+    }
 
-	public static void sort(int a[],int numberOfBuckets) {
-		int i=0;
-		for(List<Integer> bucket : createBuckets(a,numberOfBuckets)) {
-			Collections.sort(bucket);
-			for(int item:bucket)
-				a[i++]=item;
-		}
-	}
-	private static List<List<Integer>> createBuckets(int[] a,int numberOfBuckets){
-		List<List<Integer>> buckets=new ArrayList<>();
-		for(int i=0;i<numberOfBuckets;i++)
-			buckets.add(new ArrayList<>());
-		for(int item:a)
-			buckets.get(item/numberOfBuckets).add(item);
-		return buckets;
-	}
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int a[]= {7,1,5,3,2};
-		sort(a,3);
-		System.out.println("Sorted array: ");
-		for(int i:a)
-			System.out.print(i+" ");
-	}
-
+    public static void sort(int[] a) {
+        int i=0;
+        for(List<Integer> bucket : createBuckets(a,((int)maxValue(a)*a.length)+1)) {
+            Collections.sort(bucket);
+            for(int item:bucket)
+                a[i++]=item;
+        }
+    }
+    private static List<List<Integer>> createBuckets(int[] a,int numberOfBuckets){
+        List<List<Integer>> buckets=new ArrayList<>();
+        for(int i=0;i<numberOfBuckets;i++)
+            buckets.add(new ArrayList<>());
+        for(int item:a)
+            buckets.get((int)item*a.length).add(item);
+        return buckets;
+    }
 }
