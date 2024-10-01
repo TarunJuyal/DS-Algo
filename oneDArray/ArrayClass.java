2a26782bf9c2da147bec374db183dfb04610f071
Tarun Juyal <tarunJuyalHere@gmail.com>
2024-10-01 13:38:59 +0530

    Update ArrayClass.java

--- oneDArray/ArrayClass.java
+++ oneDArray/ArrayClass.java
@@ -1,125 +1,80 @@
 package oneDArray;
 
-class Array{
-	private int[] items;
-	private int size;
-	
-	public Array(int length) {
-		items=new int[length];
-	}
-	
-	public void insert(int item) {
-		//If array gets full,resize it.
-		if(items.length==size) {
-			int[] newArray=new int[2*size];
-			for(int i=0;i<size;i++) {
-				newArray[i]=items[i];
-			}
-			items=newArray;	
-		}
-		//Addition at end
-		items[size]=item;
-		size++;
-	}
-	
-	public void insertAt(int item,int index) {
-		//Insert at a position already present.
-		if(index<0||index>=size) {
-			throw new IllegalArgumentException();
-		}
-		//If array gets full,resize it.
-		if(items.length==size) {
-			int[] newArray=new int[2*size];
-			for(int i=0;i<size;i++) {
-				newArray[i]=items[i];
-			}
-			items=newArray;	
-			}
-		if(index==size) {
-			items[index]=item;
-		}else {
-			for(int i=size;i>index;i--) {
-				items[i]=items[i-1];
-			}
-			items[index]=item;
-		}
-		size++;
-	}
-	
-	public void removeAt(int index) {
-		if(index<0||index>=size) {
-			throw new IllegalArgumentException();
-		}
-		for(int i=index;i<size;i++) {
-			items[i]=items[i+1];
-		}
-		size--;
-	}
-	
-	public int indexOf(int item) {
-		for(int i=0;i<size;i++) {
-			if(items[i]==item)
-				return i;
-		}
-		return -1;
-	}
-	
-	public int max() {
-		int max=items[0];
-		for(int i=1;i<size;i++) {
-			if(items[i]>max)
-				max=items[i];
-		}
-		return max;
-	}
-	
-	public void reverse() {
-		for(int i=0,j=size-1;i<=j;i++,j--) {
-			int temp=items[i];
-			items[i]=items[j];
-			items[j]=temp;
-		}
-	}
-	
-	public void intersect(int a[]) {
-		boolean flag=false;
-		System.out.println("Intersections");
-		for(int i=0;i<size;i++) {
-			for(int j:a) {
-				if(items[i]==j) {
-					flag=true;
-					System.out.print(items[i]+" ");
-				}
-			}
-		}
-		if(!flag)
-			System.out.println("No intersections");
-	}
-	
-	public void print() {
-		for(int i=0;i<size;i++) {
-			System.out.println(items[i]);
-		}
-	}
-}
+public class MyArray {
+    private int[] items;
+    private int size;
 
-public class ArrayClass {
+    public MyArray(int length){
+        this.items= new int[length];
+        this.size=0;
+    }
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Array numbers=new Array(3);
-		numbers.insert(10);
-		numbers.insert(20);
-		numbers.insert(30);
-		numbers.insert(40);
-		numbers.removeAt(2);
-		numbers.print();
-		System.out.println("index: "+numbers.indexOf(30));
-		System.out.println("max: "+numbers.max());
-		numbers.reverse();
-		numbers.insertAt(50, 1);
-		numbers.print();
-		numbers.intersect(new int[]{50,90,80,40});
-	}
+    private void increaseCapacity(){
+        System.out.println("Capacity increased");
+        int[] newArray=new int[2*this.size];
+        for(int i=0;i<this.size;i++) {
+            newArray[i]=this.items[i];
+        }
+        this.items=newArray;
+    }
 
+    public void insert(int item){
+        //  check if array full if so double size and copy
+        if(items.length==this.size){
+            increaseCapacity();
+        }
+        //  add item
+        items[this.size++]=item;
+    }
+
+    public void insertAt(int index, int item){
+        //  check if valid index
+        if(index<0 || index>=this.size) throw new IllegalArgumentException();
+        //  if valid index then check if array is full if so increase capacity
+        increaseCapacity();
+        //  if have to insert at last then insert
+        if(index==this.size) {
+            this.items[index]=item;
+        }
+        //  if insert at other than last shift all to right then insert
+        else{
+            for (int i = size; i >index ; i--) {
+                this.items[i]=this.items[i-1];
+            }
+            this.items[index]=item;
+        }
+        this.size++;
+    }
+
+    public int indexOf(int item) {
+        for(int i=0;i<this.size;i++) {
+            if(items[i]==item)
+                return i;
+        }
+        return -1;
+    }
+
+    public boolean isEmpty() {
+        return size==0;
+    }
+
+    public int removeAt(int index){
+        //  check valid index
+        if(index<0 || index>=this.size) throw new IllegalArgumentException();
+        //  remove ie shift items to left and decrease size
+        int removedItem = this.items[index];
+        for (int i = index; i < size; i++) {
+            this.items[i]=this.items[i+1];
+        }
+        this.size--;
+        return removedItem;
+    }
+
+    public void print() {
+        System.out.print("[");
+        for(int i=0;i<this.size;i++) {
+            System.out.print(this.items[i]+" ");
+        }
+        System.out.print("]");
+    }
 }
